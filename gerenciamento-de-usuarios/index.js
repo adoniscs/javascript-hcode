16bc6b6c4ebe82d9828abf3c62e53d9b5538fb84
Adonis Cipriano <desenvolvimento777@gmail.com>
2021-07-29 23:09:30 -0300

    manipulating the DOM

--- gerenciamento-de-usuarios/index.js
+++ gerenciamento-de-usuarios/index.js
@@ -1,1 +1,16 @@
-alert("Olá mundo")
+const fields = document.querySelectorAll("#form-user-create [name]");
+const user = {};
+
+// percorre todos os campos do folumário
+// e verifica o campo do tipo radio que está checked
+fields.forEach((field) => {
+  if (field.name === "gender") {
+    if (field.checked) {
+      user[field.name] = field.value;
+    }
+  } else {
+    user[field.name] = field.value;
+  }
+});
+
+console.log(user);
