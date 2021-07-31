6910d19136745cffbcd3630ce74edd8c7dd09152
Adonis Cipriano <desenvolvimento777@gmail.com>
2021-07-31 01:51:34 -0300

    updating registration data

--- gerenciamento-de-usuarios/controllers/UserController.js
+++ gerenciamento-de-usuarios/controllers/UserController.js
@@ -116,37 +116,58 @@ class UserController {
   // método que cria o layout apos os campos serem
   // preenchidos atraves de um objeto
   addLine(dataUser) {
-    this.tableEl.insertAdjacentHTML(
-      "beforeend",
-      `
-    <tr>
-      <td>
-        <img
-          src="${dataUser.photo}"
-          alt="User Image"
-          class="img-circle img-sm"
-        />
-      </td>
-      <td>${dataUser.name}</td>
-      <td>${dataUser.email}</td>
-      <td>${dataUser.admin ? "Sim" : "Não"}</td>
-      <td>${Utils.dateFormat(dataUser.register)}</td>
-      <td>
-        <button
-          type="button"
-          class="btn btn-primary btn-xs btn-flat"
-        >
-          Editar
-        </button>
-        <button
-          type="button"
-          class="btn btn-danger btn-xs btn-flat"
-        >
-          Excluir
-        </button>
-      </td>
-    </tr>
-  `
-    );
+    let tr = document.createElement("tr");
+
+    tr.dataset.user = JSON.stringify(dataUser);
+
+    tr.innerHTML = `
+        <tr>
+          <td>
+            <img
+              src="${dataUser.photo}"
+              alt="User Image"
+              class="img-circle img-sm"
+            />
+          </td>
+          <td>${dataUser.name}</td>
+          <td>${dataUser.email}</td>
+          <td>${dataUser.admin ? "Sim" : "Não"}</td>
+          <td>${Utils.dateFormat(dataUser.register)}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-primary btn-xs btn-flat"
+            >
+              Editar
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger btn-xs btn-flat"
+            >
+              Excluir
+            </button>
+          </td>
+        </tr>
+      `;
+
+    this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount() {
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach((tr) => {
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if (user._admin) numberAdmin++;
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
   }
 }
